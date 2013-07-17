acec519f4c7dec7636144afdff66bf8b5523b6f7
Maxim Bogdanov <sin666m4a1fox@gmail.com>
2013-07-17 16:05:34 +0300

    Add --url option to grader file_3

--- grader.js
+++ grader.js
@@ -92,29 +92,21 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_file>', 'Path to url of a file', clone(assertURLExists))
+        .option('-u, --url <url>', 'path to herokuapp url', clone(assertURLExists))
+// .option('-u, --url <url>', 'path to herokuapp url', clone(assertURLExists), CHECKSURL_DEFAULT)
         .parse(process.argv);
 
-    // If file name was given, compare checks to file
-    if(!program.url) {
+    if(!program.url){ //original logic. file existing - index.html
         var checkJson = checkHtmlFile(program.file, program.checks);
         var outJson = JSON.stringify(checkJson, null, 4);
-        console.log(outJson);
-    }
+        console.log("required.main if : \n" + outJson);
+    } else { // added
+// rest.get(program.url).on('complete', function(result){
+        var checkJson = checkURL(program.url, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log("require.main else : \n" + outJson);
+    };
 
-    // If URL was given, compare checks to URL
-    else {
-        // Compare checks to URL
-        rest.get(program.url).on('complete', function(result) {
-            // result is the info in URL
-            var checkJson = checkURL(result, program.checks);
-            var outJson = JSON.stringify(checkJson, null, 4);
-
-            console.log(outJson);
-        });
-    }
-}
-
-else {
+} else {
     exports.checkHtmlFile = checkHtmlFile;
 }
