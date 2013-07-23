3eeacfa2d653abbaba1976a699f48d9c8a99c715
Maxim Bogdanov <sin666m4a1fox@gmail.com>
2013-07-23 12:14:00 +0300

    Merge

--- web.js
+++ web.js
@@ -1,17 +1,21 @@
-var express = require('express');
-var fs = require('fs');
+var fs = require('fs'),
+    express = require('express'),
+    app = express.createServer(express.logger()),
+    port = process.env.PORT || 8080;
 
 
-var app = express();
-app.use('/static', express.static(__dirname + '/public/img/link_box_meaning.png'));
+app.use(express.static(__dirname + '/public/'));
 
-var text = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-    var buffer = new Buffer(text, 'utf-8');
-    response.send(buffer.toString('utf8'));
+    var file = fs.readFile('public/index.html', function(err, data) {
+        if (err) throw new Error('Error in loading file');
+        response.send(data.toString('utf-8'));
+    });
+
 });
 
-var port = process.env.PORT || 5000;
+
+
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
